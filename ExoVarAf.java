063497b4c27cf4f07449c091e9c5d4538b405bc0
Helene <helenerasoavelo@gmail.com>
2025-04-17 22:37:45 +0300

    exercise:variables and display.

---
+++ ExoVarAf.java
@@ -0,0 +1,11 @@
+public class ExoVarAf {
+    public static void main(String[] args){
+        String nom = "Hélene";
+        int age = 28;
+        boolean aimeJava = true;
+
+        System.out.println("je m'appelle " + nom + ".");
+        System.out.println("j'ai " + 28 + "ans.");
+        System.out.println("Est-ce que j'aime java? " + aimeJava);
+    }
+}
