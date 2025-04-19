b874df6ab12bac8ca0f0945eccbb80d156b23c0d
Helene <helenerasoavelo@gmail.com>
2025-04-19 21:10:03 +0300

    feat:Applicated "else if"

---
+++ MineurOuMajeur/MineurOuMajeur.java
@@ -0,0 +1,15 @@
+public class MineurOuMajeur {
+    public static void main(String[] arg) {
+        int age = 10;
+
+        if(age<18){
+            System.out.println("elle est mineur.");
+        }else if(age>=18 && age<=60){
+            System.out.println("elle est majeur");
+        }else{
+            System.out.println("elle vieux");
+        }
+       
+
+    }
+}
